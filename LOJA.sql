ebf62cf5c9cf7026f96e2232c30f3dc5d4f01333
maicondm2022 <105238019+maicondm2022@users.noreply.github.com>
2025-10-16 16:38:34 -0300

    Update LOJA.sql

--- LOJA.sql
+++ LOJA.sql
@@ -15,8 +15,36 @@ foreign key (id_cliente) references Loja_clientes (id_c)
 -- importacao  
 set global local_infile = 1;
 
-load data local infile 'C:/Users\tecmd\OneDrive\Documentos\Clientes1.csv'
+load data local infile 'C:/Clientes.csv'
 into table Loja_clientes
 fields terminated by';'
 lines terminated by '\r\n'
 ignore 1 rows;
+select * from Loja_clientes;
+-- importacao
+load data local infile 'C:/Pedidos.csv'
+into table Loja_pedido
+fields terminated by';'
+lines terminated by '\r\n'
+ignore 1 rows;
+select * from Loja_pedido;
+select nome,email from Loja_clientes;
+select produto,data_pedido from Loja_pedido;
+select nome,email from Loja_clientes where cidade='campina grande';
+ select * from Loja_pedido where id_cliente <>2; 
+ -- = igual
+ -- > maior
+ -- < menor
+ -- >=maior igual
+ -- <menor igual
+ -- <> diferente 
+ select nome from Loja_clientes order by nome asc;-- ordem ascendente
+ select nome from Loja_clientes order by nome desc;-- ordem descendente
+ select distinct cidade from Loja_clientes; -- item repetidos ele nao traz no relatorio,1 dado de cada vez
+ select *from Loja_pedido limit 3;-- limite de ' n' pedidos
+ select *from Loja_pedido where data_pedido > '2025-10-13' limit 3; -- limit = limitado a 
+ select nome from Loja_clientes where nome like '%a';-- mostrar nome com a like= geral 
+
+ select c.nome ,p.produto,p.data_pedido
+ from Loja_pedido as p
+ join Loja_clientes as c on p.id_cliente= c.id_c;
